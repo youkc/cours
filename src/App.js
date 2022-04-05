3176744e8ca5dd5b64d5ebed57a9219b10dffe03
youssoupha@yux.design <youssoupha@yux.design>
2022-04-05 10:25:16 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="conatiner">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="text-center">
+              <img src={logo} className="App-logo my-4" alt="logo" />
+              <h3 className="mb-4 text-white">Example de formulaire</h3>
+            </div>
+          </div>
+          <div class="col-md-6 offset-md-3">
+            <div class="card shadow cardform">
+              <div class="card-body">
+                <form class="row g-3">
+                  <div class="col-md-6">
+                    <label for="inputEmail4" class="form-label">
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="inputEmail4"
+                    />
+                  </div>
+                  <div class="col-md-6">
+                    <label for="inputPassword4" class="form-label">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      id="inputPassword4"
+                    />
+                  </div>
+                  <div class="col-12">
+                    <label for="inputAddress" class="form-label">
+                      Address
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="inputAddress"
+                      placeholder="1234 Main St"
+                    />
+                  </div>
+                  <div class="col-12">
+                    <label for="inputAddress2" class="form-label">
+                      Address 2
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="inputAddress2"
+                      placeholder="Apartment, studio, or floor"
+                    />
+                  </div>
+                  <div class="col-12">
+                    <button type="submit" className="btn btn-primary">
+                      Sauvegarder
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
